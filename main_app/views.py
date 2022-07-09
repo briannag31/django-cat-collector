5c604809155b6cabfaf76f34326c909706b519d1
Brianna Gaines <brianna@Briannas-MacBook-Air.local>
2022-07-09 17:27:52 -0600

    end of day 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
 # Add the Cat class & list and view function below the imports
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+# class Cat:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
 
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+# cats = [
+#   Cat('Lolo', 'tabby', 'foul little demon', 3),
+#   Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello(╭ರᴥ•́)</h1>')
+  return HttpResponse('<h1>Hello(╭ರᴥ•́)</h1>')
 
 def about(request):
-    return render(request, "about.html")
+  return render(request, "about.html")
 
 def cats_index(request):
-    return render(request, 'cats/index.html', { 'cats': cats })
+  cats = Cat.objects.all()
+  #could do Cat.objects.order_by("id")and this would make them by id, but because we ket changing the variable, it acts like the last added is #1
+  return render(request, 'cats/index.html', { 'cats': cats })
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', { 'cat': cat })
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
